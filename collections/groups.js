38d1c0abc103cf807de54b3f494181d2f60f99eb
Mike Errington <mike.errington@gmail.com>
2015-11-11 11:26:24 -0500

    Method to create new groups

--- collections/groups.js
+++ collections/groups.js
@@ -1,1 +1,13 @@
 Groups = new Mongo.Collection('groups');
+
+Meteor.methods({
+	'newGroup': function(group) {
+		Groups.insert(group, function(error, id) {
+			if (error) {
+				throw "Error creating";
+			} else {
+				return id;
+			}
+		});
+	}
+})
