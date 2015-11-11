d46bebc8cbe72c6a2c478771e79f6ef0ea49b247
Mike Errington <mike.errington@gmail.com>
2015-11-11 12:09:56 -0500

    Cleaning up code for creating new group

--- client/groups/new/new.js
+++ client/groups/new/new.js
@@ -1,15 +1,6 @@
 Template.newGroup.events({
 	'click #createGroupBtn': function(event) {
-		group = {
-			name: $('#groupName').val(),
-			members: [{
-				'id': Meteor.userId(),
-				'points': 0,
-				'votes': 0,
-				'roles': ['owner']
-			}]
-		}
-		Meteor.call('newGroup', group, function(error, id) {
+		Meteor.call('newGroup', $('#groupName').val(), function(error, id) {
 			if (error) {
 				Materialize.toast('Error creating group, please try again!', 4000);
 			} else {
@@ -17,4 +8,4 @@ Template.newGroup.events({
 			}
 		});
 	}
-})
+});
--- collections/groups.js
+++ collections/groups.js
@@ -1,8 +1,18 @@
 Groups = new Mongo.Collection('groups');
 
 Meteor.methods({
-	'newGroup': function(group) {
-		Groups.insert(group, function(error, id) {
+	'newGroup': function(name) {
+		group = {
+			name: name,
+			members: [{
+				'id': this.userId,
+				'points': 0,
+				'votes': 0,
+				'roles': ['owner']
+			}]
+		}
+
+		return Groups.insert(group, function(error, id) {
 			if (error) {
 				throw "Error creating";
 			} else {
