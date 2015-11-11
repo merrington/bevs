f2bf492cfcbf82a2905e5aef59fef953c3bb4ddb
Mike Errington <mike.errington@gmail.com>
2015-11-11 11:24:29 -0500

    Change to new groups

--- client/groups/new/new.js
+++ client/groups/new/new.js
@@ -2,14 +2,14 @@ Template.newGroup.events({
 	'click #createGroupBtn': function(event) {
 		group = {
 			name: $('#groupName').val(),
-			owner: Meteor.userId(),
 			members: [{
 				'id': Meteor.userId(),
 				'points': 0,
-				'votes': 0
+				'votes': 0,
+				'roles': ['owner']
 			}]
 		}
-		Groups.insert(group, function(error, id) {
+		Meteor.call('newGroup', group, function(error, id) {
 			if (error) {
 				Materialize.toast('Error creating group, please try again!', 4000);
 			} else {
