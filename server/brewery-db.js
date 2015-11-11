cc75aae0f6abb0fd4ae7cbd1842a05e449a4c021
Mike Errington <mike.errington@gmail.com>
2015-11-11 12:09:26 -0500

    Checking that search returned results

--- server/brewery-db.js
+++ server/brewery-db.js
@@ -9,13 +9,15 @@ Meteor.methods({
 		});
 
 		beerResults = beerResults.data.data;
-		for (i=0; i<beerResults.length; i++) {
-			results.push({
-				'id': beerResults[i].id,
-				'name': beerResults[i].name,
-				'style': beerResults[i].style && beerResults[i].style.shortName,
-				'image': beerResults[i].labels && beerResults[i].labels.large
-			});
+		if (beerResults) {
+			for (i=0; i<beerResults.length; i++) {
+				results.push({
+					'id': beerResults[i].id,
+					'name': beerResults[i].name,
+					'style': beerResults[i].style && beerResults[i].style.shortName,
+					'image': beerResults[i].labels && beerResults[i].labels.large
+				});
+			}
 		}
 
 		console.log(results);
