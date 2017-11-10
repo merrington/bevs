6b7d671e74607dabaad87efb8cd7103a618fe7ba
Mike Errington <mike.errington@gmail.com>
2017-11-10 13:40:17 -0500

    Add number of wins, fix ordering of users

--- imports/ui/components/season/standings/Standings.jsx
+++ imports/ui/components/season/standings/Standings.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
+import get from 'lodash/get';
 
 class Standings extends React.Component {
   constructor(props) {
     super(props);
-    this.renderSeasonUsers = this.renderSeasonUsers.bind(this);
   }
 
-  renderSeasonUsers() {
-    getUserSeason = user =>
-      user.seasons.find(season => season.slug === this.props.season.slug);
+  sortUsersByPoints = (u1, u2) => {
+    return getUserSeason(u2).points - getUserSeason(u1).points;
+  };
 
-    return this.props.users
-      .sort((u1, u2) => getUserSeason(u1).points - getUserSeason(u2).points)
-      .map((user, idx) => {
-        const userSeason = getUserSeason(user);
-        return (
-          <tr key={user._id}>
-            <td>{idx + 1}</td>
-            <td>{user.profile.name}</td>
-            <td>{userSeason.points}</td>
-            <td colSpan={4} />
-          </tr>
-        );
-      });
-  }
+  winsForUser = user => {
+    const history = get(this.props.season, 'history', []);
+
+    const wins = history.filter(history => {
+      const winningBeers = history.winningBeer;
+      if (winningBeers.length === 1) {
+        const winningUsers = winningBeers[0].highest;
+        if (winningUsers.length === 1) {
+          return winningUsers[0].user === user.id;
+        }
+      }
+      return false;
+    });
+
+    return wins.length;
+  };
 
   render() {
     return (
@@ -41,7 +43,22 @@ class Standings extends React.Component {
               <th>Longest Lose Streak</th>
             </tr>
           </thead>
-          <tbody>{this.renderSeasonUsers()}</tbody>
+          <tbody>
+            {this.props.users.sort(this.sortUsersByPoints).map((user, idx) => {
+              const userSeason = user.seasons.find(
+                season => season.slug === this.props.season.slug
+              );
+              return (
+                <tr key={idx}>
+                  <td>{idx + 1}</td>
+                  <td>{user.profile.name}</td>
+                  <td>{userSeason.points}</td>
+                  <td>{this.winsForUser(user)}</td>
+                  <td colSpan={3} />
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     );
